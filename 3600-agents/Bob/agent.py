3771b201ee0f6278832faee7f44e580a0b619b35
nishia1 <agrawalnishi007@gmail.com>
2026-04-16 11:50:32 -0400

    see notes.md for current progress and details

---
+++ 3600-agents/Bob/agent.py
@@ -0,0 +1,55 @@
+from collections.abc import Callable
+from typing import List, Set, Tuple
+import random
+import jax.numpy as jnp
+
+from game import board, move, enums
+
+class RatHMM:
+    def update(self, sensor_data):
+        # update probabilities of the val
+        # for each cell in board, update probability by doing rat
+
+    def __init__(self, board, transition_matrix=None):
+        # make a jax array of all equal probabilities for each cell in the board inititally
+        self.val = jnp.ones((board.size, board.size)) / (board.size * board.size)
+        # take the array and make all probabilities equal
+        self.val = self.val / jnp.sum(self.val)
+        self.transition_matrix = transition_matrix
+
+
+class PlayerAgent:
+    """
+    /you may add and modify functions, however, __init__, commentate and play are the entry points for
+    your program and should not be changed.
+    """
+
+    def __init__(self, board, transition_matrix=None, time_left: Callable = None):
+
+        """
+        TODO: Your initialization code below. Should be used to do any setup you want
+        before the game begins (i.e. calculating priors.)
+        """
+        # call rat init code
+        self.rat_hmm = RatHMM(board, transition_matrix)
+        pass
+        
+    def commentate(self):
+        """
+        Optional: You can use this function to print out any commentary you want at the end of the game.
+        """
+        return "nishi is trying really hard to not go clinically insane"
+
+    def play(
+        self,
+        board: board.Board,
+        sensor_data: Tuple,
+        time_left: Callable,
+    ):
+        """
+        TODO: Below is random mover code. Replace it with your own.
+        You may do so however you like, including adding extra functions,
+        variables. Return a valid move from this function.
+        """
+        moves = board.get_valid_moves()
+        return random.choice(moves)
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CS3600TournamentBot
 WE WILL BEAT CARRIE
+
+# batch script commands
+running a matchup batch (runs xy as x vs y and xy as y vs x)
+python .\engine\batch_match_report.py run AgentX AgentY --repeats XY --reverse
+
+analyze all saved matches
+python .\engine\batch_match_report.py analyze --pattern *.json
+
+analyze all matches related to some bot
+python .\engine\batch_match_report.py analyze --pattern AgentX.json
